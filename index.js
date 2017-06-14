5549438f04ec9c8fa7116a7c135bdaa6760f070e
luventa <silentmic@qq.com>
2017-06-14 17:04:20 +0800

    refactor and implement functionalities

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const SMQ  = require('./lib')
-const logger = require('./lib/utils/logger').getInstance()
 
 module.exports = {
-  createClient: (opts = {
-    qq: 'unknown',
-    qrpath: __dirname
-  }) =>  {
-    return new SMQ(opts)
+  createClient: (config) =>  {
+    return new SMQ(config)
   }
 }
